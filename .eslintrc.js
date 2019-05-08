8b3032a59cdad812d5068f424090416fe18cb340
JaquelineGDS <jaqueline_gd@outlook.com>
2019-05-08 01:21:25 -0300

    refatoração

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,108 +1,61 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'camalCase': [
-          'error',
-        ],
-        'id-length': [
-          "error",
-          { "min": 1 }
-        ],
-        'keyword-spacing': [
-            'error',
-            { 'after': true,
-              'before': true
-            }
-        ],
-        'space-infix-ops': [
-            'error',
-            { 'int32Hint': false}
-        ],
-        'comma-spacing': [
-            'error',
-             { 'before': false,
-               'after': true }
-        ],
-        'space-before-blocks': [
-           'error',
-        ],
-        'spaced-comment': [
-            'error',
-            'always'
-        ],
-        'beforeColon': [
-            true
-        ],
-        'afterColon': [
-            true
-        ],
-        'no-multi-spaces': [
-            'error'
-        ],
-        'space-in-parens': [
-            'error',
-            'never'
-        ],
-        'space-before-function-paren': [
-            'error',
-            'never'
-        ],
-        'func-call-spacing': [
-            'error',
-            'never'
-        ],
-        'no-multiple-empty-lines': [
-            'error',
-            {'max': 0 }
-        ],
-        'padded-blocks': [
-            'error',
-            {'blocks': 'never'}
-        ],
-        'no-floating-decimal': [
-            'error'
-        ],
-        'object-property-newline': [
-            'error'
-        ],
-        'brace-style': [
-            'error'
-        ],
-        'eqeqeq': [
-            'error',
-             'always',
-             {"null": "ignore"}
-        ],
-        'no-array-constructor': [
-            'error',
-        ]
-    }
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'id-length': [
+      'error',
+      { 'exceptions': ['x'] }
+    ],
+    'camelcase': 2,
+    'keyword-spacing': 2,
+    'space-infix-ops': 2,
+    'comma-spacing': 2,
+    'space-before-blocks': 2,
+    'spaced-comment': 2,
+    'key-spacing': 2,
+    'no-multi-spaces': 2,
+    'space-in-parens': 2,
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ],
+    'func-call-spacing': 2,
+    'no-multiple-empty-lines': 2,
+    'padded-blocks': [
+      'error',
+      'never'
+    ],
+    'no-floating-decimal': 2,
+    'object-property-newline': 2,
+    'brace-style': 2,
+    'eqeqeq': 2,
+    'no-array-constructor': 2
+  }
 };
--- index.js
+++ index.js
@@ -1,73 +1,69 @@
-function nineDigitsCpf(strCPF) {
-  return strCPF.slice(0, 9);
+function digitValidator(digitValidator) {
+  if (digitValidator === 10 || digitValidator === 11) {
+    return 0;
+  } return digitValidator;
 }
-function transformCPFinArray(strCPF) {
-  return strCPF.split('');
+function mod11(valueMult) {
+  return valueMult % 11;
+}
+function multiplicationValueReduced(sumValue) {
+  return sumValue * 10;
+}
+function sumValue(ArrayCpf) {
+  return ArrayCpf.reduce((total, element) => {
+    return total + element;
+  });
 }
 function digitsCPFMultiplication(strCPF, total) {
   return strCPF.map((element) => {
     return element * (total--);
   });
 }
-function valueReduced(mapArrayCpf) {
-  return mapArrayCpf.reduce((total, element) => {
-    return total + element;
-  });
-}
-function multiplicationValueReduced(valueReduced) {
-  return valueReduced * 10;
-}
-function mod11(valueMult) {
-  return valueMult % 11;
-}
-function testDigitValidator(digitValidator) {
-  if (digitValidator === 10 || digitValidator === 11) {
-    return 0;
-  } else {
-    return digitValidator;
-  }
-}
-function validationDigit(cpfArray, valueToMultiply) {
-  return testDigitValidator(mod11(multiplicationValueReduced(valueReduced(digitsCPFMultiplication(cpfArray, valueToMultiply)))));
-}
 function compareDigits(firstDigitChecker, secondtDigitChecker, strCPF) {
   if (firstDigitChecker === parseInt(strCPF.charAt(9)) && secondtDigitChecker === parseInt(strCPF.charAt(10))) {
     return true;
-  } else {
-    return false;
   }
 }
-function repeatingNumbersCpf(strCPF) {
-  let cpfInArray = transformCPFinArray(strCPF);
-  return cpfInArray.every((elem) => elem === strCPF[0]);
+function validationDigit(cpfArray, valueToMultiply) {
+  const digitsCpfMultiplication = digitsCPFMultiplication(cpfArray, valueToMultiply);
+  const sumDigitsValue = sumValue(digitsCpfMultiplication);
+  const multsum = multiplicationValueReduced(sumDigitsValue);
+  const module11 = mod11(multsum);
+  return digitValidator(module11);
+}
+function transformCPFinArray(strCPF) {
+  return strCPF.split('');
+}
+function nineDigitsCpf(strCPF) {
+  return strCPF.slice(0, 9);
 }
 function checkerCPF(strCpf) {
-  let nineDigitS = nineDigitsCpf(strCpf);
-  let cpfArray = transformCPFinArray(nineDigitS);
-  let firstDigitChecker = validationDigit(cpfArray, 10);
-  let secondtDigitChecker = validationDigit(cpfArray.concat(firstDigitChecker), 11);
+  const nineDigits = nineDigitsCpf(strCpf);
+  const cpfArray = transformCPFinArray(nineDigits);
+  const firstDigitChecker = validationDigit(cpfArray, 10);
+  const secondtDigitChecker = validationDigit(cpfArray.concat(firstDigitChecker), 11);
   return compareDigits(firstDigitChecker, secondtDigitChecker, strCpf);
 }
-function validFormatCPF(strCpf) {
-  let formatCorrectCPF = RegExp(/^([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11})/g);
-  let formatCPF = strCpf.match(formatCorrectCPF);
-  return formatCPF ? true : false;
+function repeatedNumbers(strCPF) {
+  const cpfInArray = transformCPFinArray(strCPF);
+  return cpfInArray.every((elem) => elem === strCPF[0]);
 }
 function replaceCaracteres(strCpf) {
   return strCpf.replace(/[-.\\\\/]/gi, '');
 }
+function validFormatCPF(strCpf) {
+  const formatCorrectCPF = RegExp(/^([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11})/g);
+  const formatCPF = strCpf.match(formatCorrectCPF);
+  return formatCPF ? true : false;
+}
 function cpfValidator(strCpf) {
   if (validFormatCPF(strCpf)) {
-    let cpfDigits = replaceCaracteres(strCpf);
-    if (repeatingNumbersCpf(cpfDigits)) {
+    const cpfDigits = replaceCaracteres(strCpf);
+    if (repeatedNumbers(cpfDigits)) {
       return false;
     } else if (checkerCPF(cpfDigits)) {
       return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
+    } return false;
+  } return false;
 }
 module.exports.cpfValidator = cpfValidator;
